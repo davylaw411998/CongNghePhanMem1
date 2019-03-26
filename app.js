c871b96d333238723b21495579d69ee3343c45dc
davylaw411998 <davylaw123@gmail.com>
2019-03-26 13:16:48 +0700

    Commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express();
 
+//router.set("view engine", "ejs");
+//router.set("views", "./views");
+router.set("view engine", "pug");
+router.set("views","./views");
+
 router.get('/', function (req, res) {
     const wes ={ name:'wes',age:100,cool:true};
-   res.send('Hello World!!!');
+   //res.send('Hello World!!!');
    //res.json(wes);
+  // res.render('hello',{name:'wes',dog:req.query.dog});]
+   res.render('home');
 })
 
 module.exports=router;
